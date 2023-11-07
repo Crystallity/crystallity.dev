efcc2be721f502a9c8c8e4f2ee17fea26b8789c9
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-11-07 15:09:55 -0500

    Changed source to Canvas API

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function start() {
     let display = document.getElementById('display');
-    display.src = './something.html';
+    display.src = 'https://longleafschool.instructure.com/api/v1/announcements?context_codes%5B%5D=course_1239&access_token=17917~dtltruChlRAWnAI0EKwd4pdaf7UO0SLWzl1ROaIJIITJdVnw7hemZOqcOMbIbLhH';
 }
