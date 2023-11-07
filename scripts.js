73e9d19c6db66b187280f0481cebd1273f793a54
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-11-07 12:26:04 -0500

    Removed scripts.js, changed display src

--- scripts.js
+++
@@ -1,2 +0,0 @@
-let display = document.querySelector('#display');
-display.src = 'https://longleafschool.instructure.com/api/v1/announcements?context_codes%5B%5D=course_1239&access_token=17917~dtltruChlRAWnAI0EKwd4pdaf7UO0SLWzl1ROaIJIITJdVnw7hemZOqcOMbIbLhH';
