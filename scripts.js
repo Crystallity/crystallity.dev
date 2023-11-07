13a3d052ed4bf8a83c5d465ec13714579ea21c9a
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-11-07 14:59:37 -0500

    Move display back to index.html for debugging

--- scripts.js
+++
@@ -1,7 +0,0 @@
-let display = document.createElement('iframe');
-document.write(display);
-display.id = 'display';
-display.width = '700px';
-display.height = '700px';
-display.src = './something.html';
-document.body.appendChild(display);
