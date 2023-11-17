0b9ee336f44a67de61bdcd9ce0bed46dc30edf68
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-11-17 15:03:54 -0500

    Create home.html

--- azurelyte/pages.js
+++ azurelyte/pages.js
@@ -20,59 +20,6 @@ function overwriteCl(next) {
     }
 }
 
-let home = `
-<h1 class="docHead">AzureLyte</h1>
-<h1>Welcome to the writetest page.</h1>
-<h2>It's a page for writetesting.</h2>
-<p>Click these buttons to change page content (<strong>without</strong> changing the url!).</p>
-<button onclick="overwriteCl(page1)">Load Page 1</button>
-<button onclick="overwriteCl(page2)">Load Page 2</button>
-<details>
-    <summary>
-        Filler text
-    </summary>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <details>
-        <summary>
-            More filler text ;)
-        </summary>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <details>
-            <summary>
-                Even more filler text 0.0
-            </summary>
-            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-            <p>AAARRRRGH!!! Enough with the filler text!</p>
-        </details>
-    </details>
-</details>
-`;
-
-let changelog = `
-<style>
-    .changelogDiv {
-        border: 2px solid #909090;
-        width: 0;
-        margin: 7px;
-    }
-
-    #changelogDivLeft {
-        border-top-left-radius: 10px;
-        border-bottom-left-radius: 10px;
-    }
-</style>
-<h1 class="docHead">AzureLyte</h1>
-<div id="changelogDivLeft" class="changelogDiv"></div>
-`;
-
-function setChangelogSize() {
-    left = document.getElementById('changelogDivLeft');
-    right = document.getElementById('changelogDivRight');
-    newSize = (window.innerWidth - (left.style.margin * 2) ) / 2;
-    left.style.width = newSize;
-    right.style.width = newSize;
-}
-
 async function fetchText(source) {
     return await (await fetch(source)).text();
 }
