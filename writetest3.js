281758607a45ed81537bbb3b975d675f9895adec
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-12-02 08:16:05 -0500

    Fix content elements

--- writetest3.js
+++ writetest3.js
@@ -6,12 +6,12 @@ function loadPage(button) {
     let content1 = document.querySelector('#content1');
     let content2 = document.querySelector('#content2');
     if (button.currentTarget.id == 'prefetch') {
-        content1.src = 'testfile.mp4';
+        content1.data = 'testfile.mp4';
     } else if (button.currentTarget.id == 'noprefetch') {
-        content2.src = 'testfile2.mp4';
+        content2.data = 'testfile2.mp4';
     } else if (button.currentTarget.id == 'triggerboth') {
-        content1.src = 'testfile.mp4';
-        content2.src = 'testfile2.mp4';
+        content1.data = 'testfile.mp4';
+        content2.data = 'testfile2.mp4';
     } else {
         throw new Error('function loadPage() could not determine trigger element!');
     }
