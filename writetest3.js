a2f490d2524125ae1b850fc9a85fa226a68249c3
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-12-02 08:07:17 -0500

    Finish writetest3

--- writetest3.js
+++ writetest3.js
@@ -1,12 +1,20 @@
 function startup() {
-    document.querySelector('#button').addEventListener('onclick', loadPage)
+    document.querySelector('#prefetch').addEventListener('onclick', loadPage)
 }
 
-function loadPage() {
-    let content = document.querySelector(#content)
-    content.src = '/writetest3page.html';
-    content.type = 'text/html';
-    content.style.display = 'block';
+function loadPage(button) {
+    let content1 = document.querySelector('#content1');
+    let content2 = document.querySelector('#content2');
+    if (button.currentTarget.id == 'prefetch') {
+        content1.src = 'testfile.mp4';
+    } else if (button.currentTarget.id == 'noprefetch') {
+        content2.src = 'testfile2.mp4';
+    } else if (button.currentTarget.id == 'triggerboth') {
+        content1.src = 'testfile.mp4';
+        content2.src = 'testfile2.mp4';
+    } else {
+        throw new Error('function loadPage() could not determine trigger element!');
+    }
 }
 
 document.body.addEventListener('onload', startup);
