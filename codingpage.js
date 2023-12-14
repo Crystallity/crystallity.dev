d7eb80fd6c8b7d796c00808f390dc4787f416e34
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-12-14 13:24:51 -0500

    Wrap setup in function and add debugging

--- codingpage.js
+++ codingpage.js
@@ -1,15 +1,26 @@
-let box = document.createElement(‘textarea’);
-let button = document.createElement(‘button’);
+function setup() {
+  console.log('[codepage.js / setup] running setup function');
+  
+  let box = document.createElement(‘textarea’);
+  let button = document.createElement(‘button’);
 
-box.id = ‘box’;
-button.id = ‘button’;
-button.innerHTML = ‘load’;
-button.onclick = ‘runCode()’;
+  console.log('[codepage.js / setup] created elements: ', box, button);
 
-document.body.append(box);
-document.body.append('<br>');
-document.body.append(button);
+  box.id = ‘box’;
+  button.id = ‘button’;
+  button.innerHTML = ‘load’;
+  button.onclick = ‘runCode()’;
+
+  console.log('[codepage.js / setup] initialized element properties');
+
+  document.body.append(box);
+  document.body.append(document.createElement('br'));
+  document.body.append(button);
+
+  console.log('[codepage.js / setup] appended elements to body: ', document.body.innerHTML);
+}
 
 function runCode() {
+  console.log('[codepage.js / runCode] running eval on #box.innerText: ', document.querySelector('#box).innerText);
   eval(document.querySelector(‘#box’).innerText);
 }
