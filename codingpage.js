46179eed587745557a311730da0d788c78b75475
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-12-14 14:01:16 -0500

    Change 'innerText' to 'value'

--- codingpage.js
+++ codingpage.js
@@ -1,26 +1,26 @@
 function setup() {
-  console.log('[codepage.js / setup] running setup function');
+  console.log('[codepage / setup] running setup function');
   
   let box = document.createElement('textarea');
   let button = document.createElement('button');
 
-  console.log('[codepage.js / setup] created elements: ', box, button);
+  console.log('[codepage / setup] created elements: ', box, button);
 
   box.id = 'box';
   button.id = 'button';
   button.innerHTML = 'load';
   button.onclick = 'runCode()';
 
-  console.log('[codepage.js / setup] initialized element properties');
+  console.log('[codepage / setup] initialized element properties');
 
   document.body.appendChild(box);
   document.body.appendChild(document.createElement('br'));
   document.body.appendChild(button);
 
-  console.log('[codepage.js / setup] appended elements to body: ', document.body.innerHTML);
+  console.log('[codepage / setup] appended elements to body: ', document.body.innerHTML);
 }
 
 function runCode() {
-  console.log('[codepage.js / runCode] running eval on #box.innerText: ', document.querySelector('#box').innerText);
-  eval(document.querySelector('#box').innerText);
+  console.log('[codepage / runCode] running eval on #box.value: ', document.querySelector('#box').value);
+  eval(document.querySelector('#box').value);
 }
