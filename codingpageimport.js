7a8c11bb9f83e0dfceccbf981f522f41f6ef68ca
Crystallity <149009337+Crystallity@users.noreply.github.com>
2023-12-14 13:54:56 -0500

    Add reference import code

---
+++ codingpageimport.js
@@ -0,0 +1,8 @@
+console.log('[import] running import script');
+var code;
+code = document.createElement('script');
+console.log('[import] declared var code: ', code);
+code.src = 'https://crystallity.github.io/codingpage.js';
+console.log('[import] set code.src: ', code.src);
+document.body.appendChild(code);
+console.log('[import] appended script: ', document.body.innerHTML);
