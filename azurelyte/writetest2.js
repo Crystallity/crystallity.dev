aad96f2a7b324d2d94bd8a927889ff583b2032f6
Crystallity <149009337+Crystallity@users.noreply.github.com>
2024-01-17 15:12:46 -0500

    Delete azurelyte/writetest2.js

--- azurelyte/writetest2.js
+++
@@ -1,6 +0,0 @@
-function writeToDiv() {
-    document.querySelector("#myDiv").innerHTML += "<br>More something";
-}
-function eraseFromDiv() {
-    document.querySelector("#myDiv").innerHTML = document.querySelector("#myDiv").innerHTML.substr(0,document.querySelector("#myDiv").innerHTML.length - 5);
-}
