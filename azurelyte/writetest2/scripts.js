af6677290efe23ab954f8ce25967f8b59ad2ff62
Crystallity <149009337+Crystallity@users.noreply.github.com>
2024-01-23 14:43:10 -0500

    Add debug function

--- azurelyte/writetest2/scripts.js
+++ azurelyte/writetest2/scripts.js
@@ -1,6 +1,13 @@
 function writeToDiv() {
+    debug('writing');
     document.querySelector("#myDiv").innerHTML += "<br>More something";
 }
+
 function eraseFromDiv() {
+    debug('erasing');
     document.querySelector("#myDiv").innerHTML = document.querySelector("#myDiv").innerHTML.substr(0,document.querySelector("#myDiv").innerHTML.length - 5);
+}
+
+function debug(message) {
+    console.log('[writetest2/scripts.js]', message);
 }
